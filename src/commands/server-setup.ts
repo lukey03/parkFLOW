4cd6b0c0322c67e50bd684d327486ae0f2733c58
Luke <lukehenley@tuta.io>
2025-06-26 14:09:03 +0100

    Improve formatting of channel option setup in server-setup.ts

--- src/commands/server-setup.ts
+++ src/commands/server-setup.ts
@@ -44,7 +44,11 @@ export class ServerSetupCommand extends Command {
 				.setName(this.name)
 				.setDescription(this.description)
 				.addChannelOption((option) =>
-					option.setName('action-logs-channel').setDescription('Channel for action expiration logs (unused, feature in dev)').addChannelTypes(0).setRequired(false)
+					option
+						.setName('action-logs-channel')
+						.setDescription('Channel for action expiration logs (unused, feature in dev)')
+						.addChannelTypes(0)
+						.setRequired(false)
 				)
 				.addChannelOption((option) =>
 					option.setName('shift-logs-channel').setDescription('Channel for shift logs').addChannelTypes(0).setRequired(false)
